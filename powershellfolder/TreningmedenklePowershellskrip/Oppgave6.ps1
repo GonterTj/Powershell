4fd7a6ea80b284041b6912a291e325bb6723a9b8
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-04-27 08:20:14 +0200

    a

--- powershellfolder/TreningmedenklePowershellskrip/Oppgave6.ps1
+++ powershellfolder/TreningmedenklePowershellskrip/Oppgave6.ps1
@@ -7,3 +7,4 @@ $machinename = write-host "Machinename:" $mymachinename
 $ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
 Write-Host "Memory:" $ram "GB"
 Get-NetIPAddress | Select-Object IPAddress
+$ram2 = write-host "Memory2:" (Get-cimInstance Win32_PhysicalMemory | Measure-Object)
--- powershellfolder/TreningmedenklePowershellskrip/Oppgave7.ps1
+++ powershellfolder/TreningmedenklePowershellskrip/Oppgave7.ps1
@@ -1,2 +1,4 @@
-Clear-EventLog
+<#Clear-EventLog#>
 <#You must be a member of the administrators group on the affected computer#>
+Import-Module Microsoft.PowerShell.Management -UseWindowsPowerShell
+Get-EventLog -LogName * | % { Clear-EventLog -LogName $_.log }
